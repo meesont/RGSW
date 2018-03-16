a9e8d6860315b32fb845c661cca9d8dfbab2d06b
Tom <13624619+meesont@users.noreply.github.com>
2018-03-16 19:46:13 +0000

    intermediate 3

--- Python/Intermediate Python Stuff/Intermediate python stuff 3.py
+++ Python/Intermediate Python Stuff/Intermediate python stuff 3.py
@@ -5,11 +5,34 @@ Generators create the values on the fly (range() is a generator expression)
 List comprehension processes the entire list at once and stores it in memory, therefore
 functioning quicker however using more memory
 '''
+import functools, time
+
+
+#Timing decorator (online code)
+def timing(func):
+    @functools.wraps(func)
+    def newfunc(*args, **kwargs):
+        startTime = time.time()
+        func(*args, **kwargs)
+        elapsedTime = time.time() - startTime
+        print('[{}] finished in {} ms'.format(
+            func.__name__, int(elapsedTime * 1000)))
+    return newfunc
+
 
 #This is a generator expression
-xyz = (i for i in range(5000))
-print(list(xyz)[:5])
+@timing #timing decorator
+def generator():
+    xyz = (i for i in range(500000))
+    print(list(xyz)[:5])
+    
+
+@timing
+def comprehension():
+    abc = [i for i in range(500000)]
+    print(abc[:5])    
 
 
-abc = [i for i in range(5000)]
-print(abc[:5])
+if __name__ == '__main__':
+    generator()
+    comprehension()
