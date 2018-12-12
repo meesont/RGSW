0e03097160248ba59e95415f46170df8fe3d71b7
Tom <13624619+meesont@users.noreply.github.com>
2018-12-12 09:14:42 +0000

    Update selenium/requests

--- Python/pythonRequests.py
+++ Python/pythonRequests.py
@@ -1,7 +1,7 @@
 # # @Author: Thomas Meeson <Thomas>
 # # @Date:   10-12-2018
 # @Last modified by:   thomas
-# @Last modified time: 10-12-2018
+# @Last modified time: 11-12-2018
 # # @License: Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -16,27 +16,30 @@
 # # @Copyright: Copyright 2018 Thomas Meeson
 
 import requests as r
+from bs4 import BeautifulSoup
 
-response = r.get('https://www.hipstercode.com/')
+response = r.get('https://schoolweb.rgsw.org.uk/')
 
-# linksList = [
-# 'https://schoolweb.rgsw.org.uk/'
-# ]
-# response.encoding = 'ISO-8859-1'
-
-class LoginPage:
-    def __init__(self, siteLink, username, passworrd, token):
-        self.password = password
-        self.siteLink = siteLink
-        self.username = username
-        self.token = token
+soup = BeautifulSoup(response.content, 'html.parser')
+for link in soup.find_all('a'):
+    print(link.get('href'))
+# print(soup.prettify())
 
 
-loginPage = 'https://schoolweb.rgsw.org.uk/Login/login.aspx?prelogin=https%3a%2f%2fschoolweb.rgsw.org.uk%2f'
-loginName = 'username'
-passwordName = 'password'
+# print(response.text)
 
 outfile = '/Users/thomas/Documents/School Coding - Github/RGSW/Python/scrapingOutfile.txt'
 
-with open(outfile, 'w') as f:
-    f.write(str(response.text))
+
+#
+# schoolweb = (
+# 'username': "username",
+# 'password': "password",
+# 'token': 'unknown',
+# 'loginpage': 'https://schoolweb.rgsw.org.uk/Login/login.aspx?prelogin=https%3a%2f%2fschoolweb.rgsw.org.uk%2f'
+#
+# session = r.session()
+
+
+# with open(outfile, 'w') as f:
+#     f.write(str(response.text))
--- Python/seleniumExample.py
+++ Python/seleniumExample.py
@@ -1,13 +1,30 @@
-import time
+import time as t
 from selenium import webdriver
+from selenium.webdriver.common import action_chains, keys
 
+#
 driver = webdriver.Chrome('/Users/thomas/Documents/School Coding - Github/RGSW/Python/chromedriver')  # Optional argument, if not specified will search path.
-driver.get('http://www.google.com/xhtml');
-time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
 
-# /Users/thomas/Documents/School Coding - Github/RGSW/Python
+driver.get('https://webmail.rgsw.org.uk/owa')
+
+action = action_chains.ActionChains(driver)
+
+action.send_keys(keys.Keys.COMMAND+keys.Keys.ALT+'i')
+action.perform()
+t.sleep(3)
+
+action.send_keys(keys.Keys.ENTER)
+
+action.send_keys("document.querySelectorAll('')")
+
+user = driver.find_element_by_name('username')
+password = driver.find_element_by_name('password')
+
+user.clear()#Clears both boxes before filling
+password.clear()
+
+user.send_keys("meesont")
+password.send_keys("")
+
+# driver.find_element_by_type('submit').click()
+driver.find_element_by_xpath("//input[@type='submit']").click()
