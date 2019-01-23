08864012dee3fc9c9a4aab5a705d976a3515394d
Tom <13624619+meesont@users.noreply.github.com>
2019-01-23 08:48:12 +0000

    update

--- Python/portScanner.py
+++ Python/portScanner.py
@@ -2,17 +2,20 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-target = input('What website to scan: ')
+# target = input('What website to scan: ')
+
+target = 'https://www.hackthissite.org/'
 openPorts = []
 def pscan(port):
     try:
         con = s.connect((target, port))
         return True
+        con.close()
     except:
         return False
 
 for x in range(25):
     if pscan(x):
-        openPorts.append(x)
+        print(x);
 
 print(openPorts)
