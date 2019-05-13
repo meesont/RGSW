22ba2611ccb207bfe150d2728ae6b2c3596e2970
Tom <13624619+meesont@users.noreply.github.com>
2019-05-13 11:30:08 +0100

    Update concepts/readme

--- Python/A Level Revision/ProgrammingConcepts.py
+++ Python/A Level Revision/ProgrammingConcepts.py
@@ -1,16 +1,6 @@
-# @Author: Thomas Meeson <thomas>
-# @Date:   13-05-2019
-# @Last modified by:   thomas
-# @Last modified time: 13-05-2019
-# @License: Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# @Copyright: Copyright(c) 2019 Thomas Meeson
+# Primative data types
+char = 'a'
+int = 1
+float = 1.1
+boolean = True
+string = "hello world"
---
+++ Python/A Level Revision/README.md
@@ -0,0 +1,9 @@
+# Data types
+* Primitive data types
+    * boolean, char, int, double, float(real)
+* other data types
+    * String - A formulated array of chars
+    * DateTime - A format designed to store a specific combination of date and time
+    * Records - in python these are called tuples and is described as a 'sequence of immutable python objects'
+        * Note w/ tuples they cannot be edited and are effectively constants
+    * Arrays - A list of items, can be 2d or 1d
